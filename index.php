4f666b6a6bf8fcb990616e9eb552388c427690ae
Trịnh Công Đức <109200909+TrinhDuc01@users.noreply.github.com>
2023-03-26 15:21:45 +0700

    Them file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Trang chủ</title>
+</head>
+<body>
+	Trang chủ cửa hàng
+	<img src="./index/logo/logobinhminh.png">
+</body>
+</html>
