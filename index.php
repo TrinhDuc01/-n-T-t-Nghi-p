a4def63168091df60461f883047905cff118d152
Trịnh Công Đức <109200909+TrinhDuc01@users.noreply.github.com>
2023-03-28 17:08:19 +0700

    code giao dien

--- index.php
+++ index.php
@@ -14,6 +14,8 @@ $phong = mysqli_query($connect, "SELECT*FROM room");
 	<link rel="stylesheet" href="./index/css/header.css">
 	<link href="./icon/fontawesome-free-6.2.1-web/css/all.css" rel="stylesheet">
 	<link rel="stylesheet" href="./index/css/nav.css">
+	<link rel="stylesheet" href="./index/css/content.css">
+	<link rel="stylesheet" href="./index/css/content.css">
 </head>
 
 <body>
@@ -37,27 +39,27 @@ $phong = mysqli_query($connect, "SELECT*FROM room");
 			<li>
 				<a href="#">Sản phsadasẩm <i class="fa-solid fa-chevron-down"></i></a>
 				<ul>
-					<li><a href="">dầgdfaggh  </a>
-					<ul>
-						<li>
-							<a href="">asdasdasdsads</a>
-						</li>
-						<li>
-							<a href="">asdasdasdsads</a>
-						</li>
-						<li>
-							<a href="">asdasdasdsads</a>
-						</li>
-						<li>
-							<a href="">asdasdasdsads</a>
-						</li>
-					</ul>
-				</li>
+					<li><a href="">dầgdfaggh </a>
+						<ul>
+							<li>
+								<a href="">asdasdasdsads</a>
+							</li>
+							<li>
+								<a href="">asdasdasdsads</a>
+							</li>
+							<li>
+								<a href="">asdasdasdsads</a>
+							</li>
+							<li>
+								<a href="">asdasdasdsads</a>
+							</li>
+						</ul>
+					</li>
 					<li><a href="">dầgdfaggh</a></li>
 					<li><a href="">dầgdfaggh</a></li>
 					<li><a href="">sadasdasaggh</a></li>
-					<li><a href="">dầgdfaggh</a>	
-				</li>
+					<li><a href="">dầgdfaggh</a>
+					</li>
 				</ul>
 			</li>
 			<li><a href="">ádasdasd</a></li>
@@ -68,6 +70,41 @@ $phong = mysqli_query($connect, "SELECT*FROM room");
 			<li><a href="">ádasdasd</a></li>
 		</ul>
 	</nav>
+	<div class="content">
+		<div class="product_list">
+			<div class="product_list_title">
+				<span>Sản phẩm mới</span>
+				<a href="">Xem tất cả <i class="fa-solid fa-chevron-right"></i></a>
+			</div>
+			<div class="list">
+				<ul>
+					<?php
+					$newproduct = mysqli_query($connect, "SELECT * FROM `product` ORDER BY product_id DESC LIMIT 8");
+					while ($row = mysqli_fetch_array($newproduct)) {
+						?>
+						<li>
+							<img src="./admin/imgAdmin/<?php echo $row['product_image'] ?>" alt="Không có ảnh">
+							<div class="product_title">
+								<?php echo $row['product_name'] ?>
+							</div>
+							<span><i class="fa-regular fa-heart"></i></span>
+							<span class="product_price">
+								<?php $price = number_format($row['product_price']);
+								echo $price . 'đ'; ?>
+							</span><br>
+							<div class="chucnang">
+								<a class="themvao" href="cart.php?id=<?php echo $row['product_id'] ?>">Thêm vào giỏ</a>
+								<a class="xemthem"
+									href="./viewdetailproduct.php?idproduct=<?php echo $row['product_id'] ?>">Xem thêm</a>
+							</div>
+						</li>
+						<?php
+					}
+					?>
+				</ul>
+			</div>
+		</div>
+	</div>
 </body>
 
 </html>
